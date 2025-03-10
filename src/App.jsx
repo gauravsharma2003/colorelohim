029fa01aedee9a5aeff1357456e3aba5ff8e0de4
Gaurav Sharma <gaurav.sharma4@timesinternet.in>
2025-03-10 08:57:40 +0530

    setting this shit up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-red-500 text-white p-5 flex rounded-xl'> setting this shit up</div>
+    </>
+  )
+}
+
+export default App
